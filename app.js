62d7ce9974468851f7f22cc45422a382c3c5fe86
Anthony Shaw <anthonyshaw@apache.org>
2018-01-20 14:02:32 +1100

    update app with cache bust

--- app.js
+++ app.js
@@ -2,7 +2,8 @@ var app = angular.module('pepExplorer', []);
 app.controller('pepCtrl', function($scope, $http) {
     $scope.selected_status = 'Any';
     $scope.selected_version = 'Any';
-    $http.get("index.json?no-cache")
+    var v = Date.now();
+    $http.get("index.json?cache-bust=" + v)
     .then(function (response) {
         $scope.peps = response.data.peps;
         $scope.possible_python_versions = response.data.possible_python_versions;
