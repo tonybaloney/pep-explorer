985536cc3bd22197ebd77045300cdc418a874a4c
Anthony Shaw <anthonyshaw@apache.org>
2018-01-20 12:51:51 +1100

    add a custom filter, sort by PEP number

--- app.js
+++ app.js
@@ -3,3 +3,7 @@ app.controller('pepCtrl', function($scope, $http) {
     $http.get("index.json")
     .then(function (response) {$scope.peps = response.data.peps;});
 });
+app.filter('pepFilter', function($scope) {
+    return function(x) {
+        return x;
+    } });
