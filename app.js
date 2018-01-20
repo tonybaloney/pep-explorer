804b4303f0a2f8a54c418847f494d72b7dc6de59
Anthony Shaw <anthonyshaw@apache.org>
2018-01-20 12:37:55 +1100

    include angularjs

--- app.js
+++ app.js
@@ -1,5 +1,5 @@
 var app = angular.module('pepExplorer', []);
 app.controller('pepCtrl', function($scope, $http) {
     $http.get("index.json")
-    .then(function (response) {$scope.names = response.data.peps;});
+    .then(function (response) {$scope.peps = response.data.peps;});
 });
